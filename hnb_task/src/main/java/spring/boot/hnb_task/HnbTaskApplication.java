af3af7cd2d3bb21ebfb87f72b276324dc0d9edb7
최희선 <todaysunny612@gmail.com>
2025-01-31 17:18:43 +0900

    exam01_hs

---
+++ hnb_task/src/main/java/spring/boot/hnb_task/HnbTaskApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.hnb_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnbTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HnbTaskApplication.class, args);
+	}
+
+}
---
+++ hnb_task/src/main/java/task1/Task_1.java
@@ -0,0 +1,13 @@
+package task1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        int score = 80;
+        if(score >= 50) {
+            System.out.println("PASS");
+        }
+        else {
+            System.out.println("FAIL");
+        }
+    }
+}
